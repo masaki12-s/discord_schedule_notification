e51c149bc1d424fa88c7473f7c186321ad4b3475
ruchan12 <asam.archer1112@yahoo.ne.jp>
2022-08-24 16:50:33 +0900

    定期的に投稿する

--- post.py
+++ post.py
@@ -1,32 +1,38 @@
 from asyncio import tasks
 from datetime import datetime
 import discord
+from discord.ext import tasks,commands
 import os
 from dotenv import load_dotenv
 
 from get_from_caldav import CalDAV
 load_dotenv()
 TOKEN = os.getenv('TOKEN')
+CHANNELID = os.getenv('CHANNELID')
+
 class MyClient(discord.Client):
     async def on_ready(self):
         self.caldav = CalDAV()
-
+        self.send_schedules.start()
     async def on_message(self, message):
         # don't respond to ourselves
         if message.author == self.user:
             return
 
-        if message.content == 'ping':
-            await message.channel.send('pong')
+        if message.content == 'stop':
+            self.send_schedules.cansel()
 
 
-    @tasks.loop(seconds = 10)
+    @tasks.loop(hours=6)
     async def send_schedules(self):
         today = datetime.now()
         schedulelist = self.caldav.getSchedules(today,1)
-        print(schedulelist)
 
-        
+        channel = self.get_channel(CHANNELID)
+        await channel.send(schedulelist)
+        #print(schedulelist)
+
+
 intents = discord.Intents.default()
 intents.message_content = True
 client = MyClient(intents=intents)
