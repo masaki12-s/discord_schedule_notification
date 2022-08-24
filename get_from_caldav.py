37def62b42ca49b83ff9017e1c415995e1c34556
ruchan12 <asam.archer1112@yahoo.ne.jp>
2022-08-24 15:17:31 +0900

    get_from_caldavのクラス化

--- get_from_caldav.py
+++ get_from_caldav.py
@@ -2,42 +2,53 @@ import caldav
 import datetime
 import os
 from dotenv import load_dotenv
-load_dotenv()
 
-url = os.getenv('url')
-username = os.getenv('username')
-password = os.getenv('password')
-
-client = caldav.DAVClient(url=url, username=username, password=password)
-principal = client.principal()
-calendars = principal.calendars()
-    
 today = datetime.datetime.now()
-schedules = []
-for calendar in calendars:
-        try:
-            events = calendar.date_search(start=today, end=today+datetime.timedelta(days=1), expand=True)
-            for event in events:
-                d = {}
-                for event_data in event.data.splitlines():
-                    pair = event_data.split(":")
-                    d[pair[0]] = pair[1]
-                schedules.append(d)
-        except:
-            continue
+class CalDAV:
+    def __init__(self) -> None:
+        load_dotenv()
+        self.url = os.getenv('url')
+        self.username = os.getenv('username')
+        self.password = os.getenv('password')
+    def getSchedules(self,start:datetime,days:int):
+        with caldav.DAVClient(url=self.url, username=self.username, password=self.password) as client:
+            principal = client.principal()
+            calendars = principal.calendars()
+            end = start + datetime.timedelta(days=days)
 
-d = {}
-for schedule in schedules:
-    if 'SUMMARY' in schedule.keys():
-        key = schedule['SUMMARY']
-    if 'DTSTART' in schedule.keys():
-        value1 = schedule['DTSTART'][:8]
-    elif 'DTSTART;VALUE=DATE' in schedule.keys():
-        value1 = schedule['DTSTART;VALUE=DATE'][:8]
-    
-    if 'DTEND' in schedule.keys():
-        value2 = schedule['DTSTART'][:8]
-    elif 'DTEND;VALUE=DATE' in schedule.keys():
-        value2 = schedule['DTEND;VALUE=DATE'][:8]
-    d[key] = (value1,value2)
-print(d)
+            schedules = []
+            for calendar in calendars:
+                    try:
+                        events = calendar.date_search(start=start, end=end, expand=True)
+                        for event in events:
+                            var = calendar.name
+                            d = {}
+                            for event_data in event.data.splitlines():
+                                pair = event_data.split(":")
+                                d[pair[0]] = pair[1]
+                            schedules.append((var,d))
+                    except:
+                        continue
+            #print(schedules)
+           
+            schedulelist = []
+            for schedule in schedules:
+                var = schedule[0]
+                
+                if 'SUMMARY' in schedule[1].keys():
+                    summary = schedule[1]['SUMMARY']
+                if 'DTSTART' in schedule[1].keys():
+                    startdate = schedule[1]['DTSTART'][:8]
+                elif 'DTSTART;VALUE=DATE' in schedule[1].keys():
+                    startdate = schedule[1]['DTSTART;VALUE=DATE'][:8]
+                
+                if 'DTEND' in schedule[1].keys():
+                    enddate = schedule[1]['DTSTART'][:8]
+                elif 'DTEND;VALUE=DATE' in schedule[1].keys():
+                    enddate = schedule[1]['DTEND;VALUE=DATE'][:8]
+                schedulelist.append((var,summary,(startdate,enddate)))
+            print(schedulelist)
+
+if __name__=='__main__':
+    c = CalDAV()
+    c.getSchedules(datetime.datetime.now(),1)
