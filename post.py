bca7b6b787a72954de1b745acf9d255997319740
ruchan12 <asam.archer1112@yahoo.ne.jp>
2022-08-24 15:48:13 +0900

    定期的にスケジュールを投稿する機能の実装

--- post.py
+++ post.py
@@ -1,11 +1,15 @@
+from asyncio import tasks
+from datetime import datetime
 import discord
 import os
 from dotenv import load_dotenv
+
+from get_from_caldav import CalDAV
 load_dotenv()
 TOKEN = os.getenv('TOKEN')
 class MyClient(discord.Client):
     async def on_ready(self):
-        print('Logged on as', self.user)
+        self.caldav = CalDAV()
 
     async def on_message(self, message):
         # don't respond to ourselves
@@ -15,6 +19,14 @@ class MyClient(discord.Client):
         if message.content == 'ping':
             await message.channel.send('pong')
 
+
+    @tasks.loop(seconds = 10)
+    async def send_schedules(self):
+        today = datetime.now()
+        schedulelist = self.caldav.getSchedules(today,1)
+        print(schedulelist)
+
+        
 intents = discord.Intents.default()
 intents.message_content = True
 client = MyClient(intents=intents)
